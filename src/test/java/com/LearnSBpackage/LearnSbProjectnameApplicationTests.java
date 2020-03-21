2f9a5f1c2e15039071f59110c2d244a98e694b7b
swapgithub108 <59484438+swapgithub108@users.noreply.github.com>
2020-03-20 22:21:11 -0500

    git commit for learnSB

---
+++ src/test/java/com/LearnSBpackage/LearnSbProjectnameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnSBpackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSbProjectnameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
